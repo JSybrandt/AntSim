04b826d38b152a75a3116e35a4b99639ede93d53
LoewSD1 <LoewSD1@gcc.edu>
2014-10-19 10:43:54 -0400

    Queen
    I created Queen. Everything runs, but I'm not sure if the queen is
    actually doing anything. Let me know what else I need to do.

---
+++ ANTSIM/Queen.cpp
@@ -0,0 +1,153 @@
+#include "ant.h"
+#include "antSim.h"
+#include "Queen.h"
+
+Queen::Queen():Ant(){
+
+	age = 0;
+	AntSim* world;
+	isUnderground = true;
+	foodLevel = antNS::QUEEN_STOMACH_SIZE;
+	alive = false;
+	setActive(false);
+	signalIndex = 0;
+	behavior = Behavior::DEFAULT;
+
+	cooldown = 0;
+}
+
+Queen::~Queen(){
+
+}
+
+void Queen::create(VECTOR2 location)
+{
+	setCenterLocation(location);
+	age = 0;
+	isUnderground = true;
+	setActive(true);
+	foodLevel = antNS::QUEEN_STOMACH_SIZE;
+	alive = true;
+	health = antNS::QUEEN_MAX_HEALTH;
+}
+
+float Queen::receiveFood(float avalible)
+{
+		float emptySpace = antNS::QUEEN_STOMACH_SIZE - foodLevel;
+		avalible = min(emptySpace,avalible);
+		foodLevel += avalible;
+		return avalible;
+}
+
+void Queen::die()
+{
+	alive = false;
+	setActive(false);
+}
+
+void Queen::hungryAction(float frameTime)
+{
+	//beg
+	if(cooldown == 0){
+		world->spawnPher(*getCenter(),Signal(SignalType::queen,*getCenter()));
+		cooldown += antNS::COOLDOWN;
+	}
+
+}
+
+void Queen::defaultAction(float frameTime){
+
+	VECTOR2 location;
+	Reproduce(location);
+
+}
+
+void Queen::update(float frameTime){
+	if(getActive())
+	{
+		if(cooldown > 0) cooldown -= frameTime;
+		if(cooldown <= 0) cooldown = 0;
+
+		//age
+		age += frameTime;
+		if(age > antNS::QUEEN_LIFE_EXPECTANCY) health -= antNS::OLD_AGE_DAMAGE * frameTime;
+
+		//eat food
+		foodLevel -= antNS::METABOLISM*frameTime;
+		if(foodLevel < 0) health -= antNS::STARVATION_DAMAGE * frameTime;
+
+
+		//determine behavior
+		if(foodLevel < antNS::QUEEN_STOMACH_SIZE *0.25) behavior = Behavior::BEGGING;
+		else behavior = Behavior::DEFAULT;
+
+
+		if(behavior == Behavior::DEFAULT)
+		{
+			defaultAction(frameTime);
+		}
+		if(behavior == Behavior::BEGGING)
+		{
+			hungryAction(frameTime);
+		}
+		
+
+		//reset signals
+		for(int i = 0 ; i < antNS::NUM_SIMULTANEOUS_SIGNALS; i++)
+		{
+			signals[i] = Signal();
+		}
+
+		if(health <= 0) die();
+
+	}
+}
+
+void Queen::draw()
+{
+	if(!isUnderground && getActive()) Actor::draw();
+}
+
+bool Queen::initialize(AntSim *gamePtr, int width, int height, int ncols,TextureManager *textureM)
+{
+	world = gamePtr;
+	return Actor::initialize((Game*)gamePtr,width,height,ncols,textureM);
+}
+
+void Queen::receiveSignal(Signal s)
+{
+	for(int i = 0 ; i < antNS::NUM_SIMULTANEOUS_SIGNALS; i++)
+	{
+		if(signalIndex >= antNS::NUM_SIMULTANEOUS_SIGNALS) signalIndex = 0;
+		if(signals[signalIndex].getType() == SignalType::null)
+		{
+			signals[signalIndex] = s;
+			break;
+		}
+		signalIndex++;
+	}
+}
+
+void Queen::touches(Actor* other)
+{
+	Food* food = dynamic_cast<Food*>(other);
+	if(food != NULL)
+	{
+		//request to fill stomach
+		foodLevel += food->eat(antNS::QUEEN_STOMACH_SIZE-foodLevel);
+	}
+
+	Queen* queen = dynamic_cast<Queen*>(other);
+	if(queen != NULL)
+	{
+		if(queen->getBehavior()==Behavior::BEGGING && foodLevel > antNS::QUEEN_STOMACH_SIZE/2)
+		{
+			foodLevel -= queen->receiveFood(foodLevel-antNS::QUEEN_STOMACH_SIZE/2);
+		}
+	}
+}
+
+
+void Queen::Reproduce(VECTOR2 location){
+	 Ant::create(location);
+}
--- ANTSIM/Queen.h
+++ ANTSIM/Queen.h
@@ -1,42 +1,15 @@
 #pragma once
+#include "ant.h"
 #include "Actor.h"
 #include "signal.h"
 #include "food.h"
 
-class AntSim;
+class Queen:public Ant{
 
-namespace antNS
-{
-	const float LIFE_EXPECTANCY = 10;
-	const float QUEEN_LIFE_EXPECTANCY = 30;
-	const float METABOLISM = 20;
-	const float STOMACH_SIZE = 100;
-	const float QUEEN_STOMACH_SIZE = 200;
-	const float STARVATION_DAMAGE = 10;
-	const float OLD_AGE_DAMAGE = 10;
-	const float ANT_SPEED = 50;
-	const float ANT_MAX_HEALTH = 100;
-	const float QUEEN_MAX_HEALTH = 150;
-	const int NUM_SIMULTANEOUS_SIGNALS = 10;
-
-	const float COOLDOWN = 10;
-}
-
-enum Behavior{
-	DEFAULT,
-	EATING,
-	BEGGING,
-	REPRODUCE
-};
-
-class Ant: public Actor
-{
 private:
 	float age;
 	AntSim* world;
 	bool isUnderground;
-	bool isMale;
-	float direction;
 	float foodLevel;
 	bool alive; //becaue we might want corpses later
 	Signal signals[antNS::NUM_SIMULTANEOUS_SIGNALS];
@@ -51,10 +24,12 @@ private:
 
 
 public:
-	Ant();
-	~Ant();
-	void update(float frameTime);
+	Queen();
+	~Queen();
 	void draw();
+	void update(float frameTime);
+	void Reproduce(VECTOR2 location);
+	void create_Ant(VECTOR2 location);
 	void create(VECTOR2 location);
 	bool initialize(AntSim *gamePtr, int width, int height, int ncols,TextureManager *textureM);
 	void die();
@@ -72,7 +47,5 @@ public:
 
 	// Return age;
 	virtual float getAge()			const {return age;}
-	
-	// Return name;
-	virtual std::string getName()			const {return name;}
+
 };
